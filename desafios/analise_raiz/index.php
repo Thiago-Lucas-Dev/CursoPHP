633d83ebc58a67ae31fe5092de13f439b2895f38
Thiago Lucas <thiagolluccass@gmail.com>
2025-08-29 09:59:55 -0300

    Desafio: Analise de Raiz quadrada e cubica - inicio

---
+++ desafios/analise_raiz/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analise de Raiz Quadrada e Cubica</title>
+</head>
+<body>
+    
+</body>
+</html>
