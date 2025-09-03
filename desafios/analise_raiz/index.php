7791b53e04e579c342989403d11cf952f014037b
Thiago Lucas <thiagolluccass@gmail.com>
2025-09-03 10:39:32 -0300

    Desafio: Fazendo codigo para calculo de raizes - WIP

--- desafios/analise_raiz/index.php
+++ desafios/analise_raiz/index.php
@@ -3,9 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Analise de Raiz Quadrada e Cubica</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Analise de Raízes Quadradas e Cubicas</title>
 </head>
 <body>
-    
+    <header>
+        <h1>
+            Analise de Raízes Quadradas e Cubicas
+        </h1>
+    </header>
+    <main>
+        <h2>
+            Escolha um número
+        </h2>
+        <form action="" method="post">
+            <label for="numero">Número:</label>
+            <input type="number" name="numdigitado" id="numdigitado">
+            <input type="submit" value="Calcular Raízes">
+        </form>
+        <?php 
+        
+        if (isset($_POST["numdigitado"])) {
+            $numero = $_POST["numdigitado"];
+
+            for ($valormulti = 1; $valormulti <= $numero; $valormulti++) {
+
+                $quadrado = $valormulti * $valormulti;
+
+                echo "$quadrado<br>";
+                
+            }
+
+            
+        
+        }
+
+            
+        
+        
+        
+        
+        
+        
+        ?>
+    </main>
 </body>
 </html>
