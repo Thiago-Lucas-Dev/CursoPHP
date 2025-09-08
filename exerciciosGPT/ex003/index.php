53e3dd02066254d5b7539d59f4a2e0af4cb9bda5
Thiago Lucas <thiagolluccass@gmail.com>
2025-09-08 15:40:13 -0300

    Exercicio com GPT: Calculadora de IMC - Finalizado

--- exerciciosGPT/ex003/index.php
+++ exerciciosGPT/ex003/index.php
@@ -11,7 +11,44 @@
         <h1>Calculadora de IMC</h1>
     </header>
     <main>
+        <form action="" method="post">
+            <label for="peso">Qual seu peso (KG): </label>
+            <input type="number" name="pesodigitado" id="pesodigitado">
+            <label for="altura">Qual sua altura (M): </label>
+            <input type="text" name="alturadigitada" id="alturadigitada">
+            <input type="submit" value="Calcular">
+        </form>
+        <?php   
         
+        if (!empty($_POST["pesodigitado"]) && (!empty($_POST["alturadigitada"]))) {
+
+            $pesodigitado = $_POST["pesodigitado"];
+
+            $alturadigitada = $_POST["alturadigitada"];
+
+            $peso = str_replace(",", ".", $pesodigitado);
+            $altura = str_replace(",", ".", $alturadigitada);      
+
+            $alturaQuadrada = $altura * $altura;
+            $imc = $peso / $alturaQuadrada;
+
+            echo "Seu IMC é de: " . "<strong>", round($imc, 2), "</strong>", "<br><br>";
+
+            if ($imc < 18.5){
+                echo "Sua classificação referente as suas características é: <strong>Abaixo do peso</strong>";
+            } elseif ($imc >= 18.5 && $imc <= 24.9){
+                echo "Sua classificação referente as suas características é: <strong>Peso normal</strong>";
+            } elseif ($imc >= 25 && $imc <= 29.9){
+                echo "Sua classificação referente as suas características é: <strong>Sobrepeso</strong>";
+            } elseif ($imc >= 30 && $imc <= 34.9){
+                echo "Sua classificação referente as suas características é: <strong>Obesidade grau 1</strong>";
+            } elseif ($imc >= 35 && $Imc <= 39.9){
+                echo "Sua classificação referente as suas características é: <strong>Obesidade grau 2</strong>";
+            } elseif ($imc >= 40){
+                echo "Sua classificação referente as suas características é: <strong>Obesidade grau 3 (mórbida)</strong>";
+            }
+        }
+        ?>
     </main>
 </body>
 </html>
